1b87cdf0f39233f2dbf5814711533ed22fc23fd4
sashkatenev <tel256-32-79@yandex.ru>
2021-02-11 03:36:04 +0300

    добавляет функцию getRandomFloat в main.js

--- js/main.js
+++ js/main.js
@@ -13,3 +13,24 @@ const getRandomInt = function (lowerBound = 0, upperBound = 0) {
   }
   return result;
 }
+
+const getRandomFloat = function (lowerBound = 0, upperBound = 0, decimalPlaces = 0) {
+  let result = null;
+  if (arguments.length == 1) {
+    if (lowerBound > 0) {
+      upperBound = lowerBound;
+      lowerBound = 0;
+    }
+  }
+  if (upperBound >= lowerBound && Number.isInteger(decimalPlaces)) {
+    const rounder = Math.pow(10, decimalPlaces);
+    result = Math.random() * (upperBound - lowerBound) + lowerBound;
+    result = Math.round(result * rounder) / rounder;
+    // result = +result.toFixed(decimalPlaces);
+  }
+  return isNaN(result) ? null : result;
+}
+
+for (let i = 0; i < 10; i++) {
+  console.log(getRandomFloat(1.11, 1.12, 3));
+}
