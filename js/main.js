5c23da203827ffa4f11185a55ce7fa85458efcec
sashkatenev <tel256-32-79@yandex.ru>
2021-02-14 23:13:31 +0300

    определяет ф-и getRandomInt и getRandomFloat в стрелочной нотации

--- js/main.js
+++ js/main.js
@@ -1,27 +1,15 @@
 'use strict';
 
-const getRandomInt = function (lowerBound = 0, upperBound = 0) {
+const getRandomInt = (lowerBound = 0, upperBound = 0) => {
   let result = null;
-  if (arguments.length == 1) {
-    if (lowerBound > 0) {
-      upperBound = lowerBound;
-      lowerBound = 0;
-    }
-  }
   if (Number.isInteger(lowerBound) && Number.isInteger(upperBound) && upperBound >= lowerBound) {
     result = Math.round(Math.random() * (upperBound - lowerBound)) + lowerBound;
   }
   return result;
 }
 
-const getRandomFloat = function (lowerBound = 0, upperBound = 0, decimalPlaces = 0) {
+const getRandomFloat = (lowerBound = 0, upperBound = 0, decimalPlaces = 0) => {
   let result = null;
-  if (arguments.length == 1) {
-    if (lowerBound > 0) {
-      upperBound = lowerBound;
-      lowerBound = 0;
-    }
-  }
   if (upperBound >= lowerBound && Number.isInteger(decimalPlaces)) {
     result = Math.random() * (upperBound - lowerBound) + lowerBound;
     const rounder = Math.pow(10, decimalPlaces);
