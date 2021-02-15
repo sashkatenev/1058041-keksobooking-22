997e262720ed4c2d8ff52b46ee8fb7e7771f78bf
sashkatenev <60226999+sashkatenev@users.noreply.github.com>
2021-02-16 02:00:52 +0300

    корректирует ф-и getRandomInt и getRandomFloat

--- js/main.js
+++ js/main.js
@@ -1,32 +1,44 @@
 'use strict';
 
 const DEFAULT_LOWER_BOUND = 0;
-const DEFAULT_UPPER_BOUND = 1;
-const DEFAULT_DECIMAL_PLACES = 3;
+const DEFAULT_UPPER_BOUND = 0;
+const DEFAULT_DECIMAL_PLACES = 5;
 
-const getRandomInt = (lowerBound = DEFAULT_LOWER_BOUND, upperBound = DEFAULT_UPPER_BOUND) => {
+const getRandomInt = (...args) => {
+  let lowerBound = args[0] || DEFAULT_LOWER_BOUND;
+  let upperBound = args[1] || DEFAULT_UPPER_BOUND;
   let result = null;
+  if (args.length == 1) {
+    if (lowerBound > 0) {
+      upperBound = lowerBound;
+      lowerBound = 0;
+    }
+  }
   if (Number.isInteger(lowerBound) && Number.isInteger(upperBound) && upperBound >= lowerBound) {
     result = Math.round(Math.random() * (upperBound - lowerBound)) + lowerBound;
   }
   return result;
 }
 
-const getRandomFloat = (lowerBound = DEFAULT_LOWER_BOUND, upperBound = DEFAULT_UPPER_BOUND, decimalPlaces = DEFAULT_DECIMAL_PLACES) => {
+const getRandomFloat = (...args) => {
+  let lowerBound = args[0] || DEFAULT_LOWER_BOUND;
+  let upperBound = args[1] || DEFAULT_UPPER_BOUND;
+  const decimalPlaces = args[2] || DEFAULT_DECIMAL_PLACES;
   let result = null;
+  if (args.length == 1) {
+    if (lowerBound > 0) {
+      upperBound = lowerBound;
+      lowerBound = 0;
+    }
+  }
   if (upperBound >= lowerBound && Number.isInteger(decimalPlaces)) {
     result = Math.random() * (upperBound - lowerBound) + lowerBound;
     const rounder = Math.pow(10, decimalPlaces);
     result = Math.round(result * rounder) / rounder;
-    // result = +result.toFixed(decimalPlaces);
   }
   return isNaN(result) ? null : result;
 }
 
-// alert(getRandomInt(1, 10));
+alert(getRandomInt(1, 10));
 
-// alert(getRandomFloat(1.11, 1.12, 3));
-
-for (let i = 0; i < 10; i++) {
-  console.log(getRandomFloat());
-}
+alert(getRandomFloat(1.11, 1.12, 3));
