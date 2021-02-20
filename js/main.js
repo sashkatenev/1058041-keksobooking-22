000231860dbe7a585b37af6d9b74e6082e58a1fa
sashkatenev <60226999+sashkatenev@users.noreply.github.com>
2021-02-20 12:52:57 +0300

    определяет класс author

--- js/main.js
+++ js/main.js
@@ -3,6 +3,11 @@
 const DEFAULT_LOWER_BOUND = 0;
 const DEFAULT_UPPER_BOUND = 0;
 const DEFAULT_DECIMAL_PLACES = 5;
+const SIMILAR_AD_COUNT = 10;
+const AVATAR_FILENAME_TEMPLATE = 'img/avatars/user{xx}.png';
+const MIN_USER_NUMBER = 1;
+const MAX_USER_NUMBER = 8;
+const USER_NUMBER_DIGITS = 2;
 
 const getRandomInt = (...args) => {
   let lowerBound = args[0] || DEFAULT_LOWER_BOUND;
@@ -39,6 +44,41 @@ const getRandomFloat = (...args) => {
   return isNaN(result) ? null : result;
 }
 
-alert(getRandomInt(1, 10));
+// alert(getRandomInt(1, 10));
 
-alert(getRandomFloat(1.11, 1.12, 3));
+// alert(getRandomFloat(1.11, 1.12, 3));
+
+const zeroPad = (number, lengthNumber) => {
+  return (Array(lengthNumber).fill('0') + number).slice(-lengthNumber);
+}
+
+const getRandomAvatarFileName = (template) => {
+  let userNumber = zeroPad(getRandomInt(MIN_USER_NUMBER, MAX_USER_NUMBER), USER_NUMBER_DIGITS);
+  return `${template.replace('{xx}', userNumber)}`;
+};
+
+class Author {
+  constructor (avatar) {
+    this.avatar = avatar;
+  }
+}
+
+class SimilarNearAd {
+  constructor (author) {
+    this.author = author;
+  }
+}
+
+const createAd = () => {
+  const author = new Author(getRandomAvatarFileName(AVATAR_FILENAME_TEMPLATE));
+
+  const similarNearAd = new SimilarNearAd(
+    author
+  );
+
+  return similarNearAd;
+};
+
+let similarNearAds = new Array(SIMILAR_AD_COUNT).fill(null).map(() => createAd());
+
+console.log(similarNearAds);
