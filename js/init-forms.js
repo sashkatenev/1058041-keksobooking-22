1fde2ad86eda01be0eb66b15195a7fa77257871c
sashkatenev <60226999+sashkatenev@users.noreply.github.com>
2021-03-11 23:10:44 +0300

    добавляет activateForm() в init-forms.js

--- js/init-forms.js
+++ js/init-forms.js
@@ -15,12 +15,32 @@ const forms = {
   },
 }
 
-const initForms = () => {
-  forms['ad-form']['handle'] = setAdForm(forms['ad-form']['className']);
-}
-
-const getFormElement = (key) => {
-  return forms[key]['handle'];
+const getForm = (name) => {
+  return forms[name]['handle'];
 };
 
-export { initForms, getFormElement };
+const activateForm = (key, activateFlag) => {
+  const formElement = getForm(key);
+  const disabledFormClassName = `${forms[key]['className']}--disabled`;
+  const fieldsets = formElement.querySelectorAll('fieldset');
+  if (activateFlag) {
+    formElement.classList.remove(disabledFormClassName);
+    fieldsets.forEach((fieldset) => {
+      fieldset.removeAttribute('disabled');
+    });
+  } else {
+    formElement.classList.add(disabledFormClassName);
+    fieldsets.forEach((fieldset) => {
+      fieldset.setAttribute('disabled', '');
+    });
+  }
+}
+
+const initForms = () => {
+  forms['ad-form']['handle'] = setAdForm(forms['ad-form']['className']);
+
+  activateForm('ad-form', false);
+}
+
+
+export { initForms, getForm };
