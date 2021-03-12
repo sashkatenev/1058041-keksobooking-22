9b598e0db1546f9ec18b99b30955dcffa44587bc
sashkatenev <tel256-32-79@yandex.ru>
2021-03-12 23:08:14 +0300

    добавляет главную метку на карту

--- js/init-forms.js
+++ js/init-forms.js
@@ -65,4 +65,4 @@ const initForms = () => {
 }
 
 
-export { initForms };
+export { initForms, getForm };
--- js/map.js
+++ js/map.js
@@ -1,6 +1,7 @@
 /* global L:readonly */
 
 import { getAreaCenter } from './data.js';
+import { getForm } from './init-forms.js';
 
 const setMap = (className, loadMapHandler) => {
   const mapElement = document.querySelector(`.${className}`);
@@ -19,6 +20,28 @@ const setMap = (className, loadMapHandler) => {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     },
   ).addTo(map);
+
+  const mainPinIcon = L.icon({
+    iconUrl: '../img/main-pin.svg',
+    iconSize: [52, 52],
+    iconAnchor: [26, 52],
+  });
+
+  const mainPinMarker = L.marker(
+    getAreaCenter(),
+    {
+      draggable: true,
+      icon: mainPinIcon,
+    },
+  );
+
+  mainPinMarker.addTo(map);
+
+  mainPinMarker.on('moveend', (evt) => {
+    const lat = evt.target.getLatLng().lat;
+    const lng = evt.target.getLatLng().lng;
+    getForm('ad-form').querySelector('#address').value = `${lat}, ${lng}`;
+  });
 
   return mapElement;
 };
