d760a823f09078407c7aee34ceb3ff5f93c1dd02
sashkatenev <tel256-32-79@yandex.ru>
2021-03-16 23:31:47 +0300

    задаёт валидацию полей title и price

--- js/ad-form.js
+++ js/ad-form.js
@@ -2,24 +2,56 @@ import { getHousingMinPrice } from './data.js';
 import { getForm } from './init-forms.js';
 
 let adForm = null;
-let addressElement = null;
-let priceElement = null;
-let housingTypeElement = null;
-let timeInElement = null;
-let timeOutElement = null;
+let titleInput = null;
+let addressInput = null;
+let priceInput = null;
+let housingTypeInput = null;
+let timeInInput = null;
+let timeOutInput = null;
 
 const adFormChangeHandler = (evt) => {
-  const targetEvent = evt.target;
+  const control = evt.target;
 
-  switch (targetEvent) {
-    case housingTypeElement:
-      priceElement.min = priceElement.placeholder = getHousingMinPrice(targetEvent.value);
+  switch (control) {
+    case housingTypeInput:
+      priceInput.min = priceInput.placeholder = getHousingMinPrice(control.value);
       break;
-    case timeInElement:
-      timeOutElement.value = targetEvent.value;
+    case timeInInput:
+      timeOutInput.value = control.value;
       break;
-    case timeOutElement:
-      timeInElement.value = targetEvent.value;
+    case timeOutInput:
+      timeInInput.value = control.value;
+      break;
+  }
+};
+
+const adFormInvalidHandler = (evt) => {
+  const control = evt.target;
+
+  switch (true) {
+    case control.validity.valueMissing:
+      control.setCustomValidity('Это поле обязательно для заполнения');
+      break;
+    case control.validity.rangeOverflow:
+      control.setCustomValidity(`Максимальное значение для этого поля: ${control.max}`);
+      break;
+    case control.validity.rangeUnderflow:
+      control.setCustomValidity(`Минимальное значение для этого поля: ${control.min}`);
+      break;
+    case control.validity.tooShort:
+      control.setCustomValidity(`Это поле должно содержать не менее ${control.minLength} символов (ещё хотя бы ${control.minLength - control.value.length})`);
+      break;
+    case control.validity.tooLong:
+      control.setCustomValidity(`Это поле должно содержать не более ${control.maxLength} символов`);
+      break;
+    case control.validity.badInput:
+      control.setCustomValidity(`Это поле должно содержать число`);
+      break;
+    case (control == priceInput) && (!(priceInput.value.match('^[0-9]+$'))):   // e.g. '1e4' rejected by the server
+      control.setCustomValidity('Это поле должно содержать только цифры');
+      break;
+    default:
+      control.setCustomValidity('');
       break;
   }
 };
@@ -27,25 +59,33 @@ const adFormChangeHandler = (evt) => {
 const setAdForm = (className) => {
   adForm = document.querySelector(`.${className}`);
 
-  priceElement = adForm.querySelector('#price');
-  priceElement.max = '1000000';
+  titleInput = adForm.querySelector('#title');
 
-  housingTypeElement = adForm.querySelector('#type');
-  priceElement.min = priceElement.placeholder = getHousingMinPrice(housingTypeElement.value);
+  priceInput = adForm.querySelector('#price');
+  priceInput.min = priceInput.placeholder = getHousingMinPrice(housingTypeInput.value);
+  priceInput.max = '1000000';
 
-  timeInElement = adForm.querySelector('#timein');
-  timeOutElement = adForm.querySelector('#timeout');
+  housingTypeInput = adForm.querySelector('#type');
+
+  timeInInput = adForm.querySelector('#timein');
+  timeOutInput = adForm.querySelector('#timeout');
 
   adForm.addEventListener('change', adFormChangeHandler);
+
+  titleInput.addEventListener('invalid', adFormInvalidHandler);
+  titleInput.addEventListener('input', adFormInvalidHandler);
+
+  priceInput.addEventListener('invalid', adFormInvalidHandler);
+  priceInput.addEventListener('input', adFormInvalidHandler);
 
   return adForm;
 };
 
 const setAddressInput = ({latitude, longitude}) => {
-  if (!addressElement) {
-    addressElement = getForm('ad-form').querySelector('#address');
+  if (!addressInput) {
+    addressInput = getForm('ad-form').querySelector('#address');
   }
-  addressElement.value = `${latitude}, ${longitude}`;
+  addressInput.value = `${latitude}, ${longitude}`;
 };
 
 export { setAdForm, setAddressInput };
