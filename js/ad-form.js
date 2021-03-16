a153d2f61a3620afb42ca2dc32ee1477af7a06a3
sashkatenev <tel256-32-79@yandex.ru>
2021-03-16 15:20:02 +0300

    корректирует setAdForm() из ad-form.js

--- js/ad-form.js
+++ js/ad-form.js
@@ -1,35 +1,40 @@
 import { getHousingMinPrice } from './data.js';
 import { getForm } from './init-forms.js';
 
+let adForm = null;
 let addressElement = null;
+let priceElement = null;
+let housingTypeElement = null;
+let timeInElement = null;
+let timeOutElement = null;
+
+const adFormChangeHandler = (evt) => {
+  const targetEvent = evt.target;
+
+  switch (targetEvent) {
+    case housingTypeElement:
+      priceElement.min = priceElement.placeholder = getHousingMinPrice(targetEvent.value);
+      break;
+    case timeInElement:
+      timeOutElement.value = targetEvent.value;
+      break;
+    case timeOutElement:
+      timeInElement.value = targetEvent.value;
+      break;
+  }
+};
 
 const setAdForm = (className) => {
-  const adForm = document.querySelector(`.${className}`);
+  adForm = document.querySelector(`.${className}`);
 
-  const priceElement = adForm.querySelector('#price');
+  priceElement = adForm.querySelector('#price');
   priceElement.max = '1000000';
 
-  const housingTypeElement = adForm.querySelector('#type');
+  housingTypeElement = adForm.querySelector('#type');
   priceElement.min = priceElement.placeholder = getHousingMinPrice(housingTypeElement.value);
 
-  const timeInElement = adForm.querySelector('#timein');
-  const timeOutElement = adForm.querySelector('#timeout');
-
-  const adFormChangeHandler = (evt) => {
-    const targetEvent = evt.target;
-
-    switch (targetEvent) {
-      case housingTypeElement:
-        priceElement.min = priceElement.placeholder = getHousingMinPrice(targetEvent.value);
-        break;
-      case timeInElement:
-        timeOutElement.value = targetEvent.value;
-        break;
-      case timeOutElement:
-        timeInElement.value = targetEvent.value;
-        break;
-    }
-  };
+  timeInElement = adForm.querySelector('#timein');
+  timeOutElement = adForm.querySelector('#timeout');
 
   adForm.addEventListener('change', adFormChangeHandler);
 
