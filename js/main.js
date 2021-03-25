abe106a9928c8f1c4b5f342195979a68b4375f8a
sashkatenev <60226999+sashkatenev@users.noreply.github.com>
2021-03-26 00:42:40 +0300

    избавляется от модуля init-forms.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
-import { initForms } from './init-forms.js';
 import { enableForm } from './util.js';
 import { loadMap, showAdMarkers, setMapMarkerMoveEndHandler } from './map.js';
 import { fetchData } from './data.js';
 import { showPopup } from './custom-popup.js';
+import { setAdForm } from './ad-form.js';
 import { filterAds, setFilterChangeHandler } from './filter-form.js';
 
 const MARKERS_TO_SHOW_MAXCOUNT = 10;
-
-initForms();
 
 const fetchDataSuccessHandler = (ads) => {
   showAdMarkers(ads, MARKERS_TO_SHOW_MAXCOUNT);
@@ -20,6 +18,10 @@ const fetchDataSuccessHandler = (ads) => {
 const fetchDataErrorHandler = (err) => {
   showPopup('#error', '.error', `Ошибка загрузки данных (${err})`);
 };
+
+setAdForm();
+enableForm('ad-form', false);
+enableForm('map__filters', false);
 
 loadMap('map__canvas')
   .then(() => {
