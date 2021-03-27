a94b008df66df319611a64ebf5bcace53a06b795
sashkatenev <60226999+sashkatenev@users.noreply.github.com>
2021-03-27 10:51:50 +0300

    добавляет сортировку списка объявлений по фичам

--- js/filter-form.js
+++ js/filter-form.js
@@ -2,6 +2,8 @@ import { showAdMarkers } from './map.js';
 import { enableForm } from './util.js';
 
 const FORM_CLASS_NAME = 'map__filters';
+const MATCHING_FEATURE_RANK = 100;
+const BONUSE_FEATURE_RANK = 1;
 
 const filterForm = document.querySelector(`.${FORM_CLASS_NAME}`);
 enableForm('map__filters', false);
@@ -10,6 +12,7 @@ let housingTypeCriteria  = filterForm.querySelector('#housing-type').value;
 let priceCriteria  = filterForm.querySelector('#housing-price').value;
 let roomsCriteria = filterForm.querySelector('#housing-rooms').value;
 let guestsCriteria = filterForm.querySelector('#housing-guests').value;
+let featuresCriteria = [];
 
 const setFilterChangeHandler = (data, maxCount) => {
   filterForm.addEventListener('change', (evt) => {
@@ -20,12 +23,20 @@ const setFilterChangeHandler = (data, maxCount) => {
 };
 
 const filterAds = (ads) => {
-  return ads.slice().filter((value) => {
-    return ((value.offer.type === housingTypeCriteria) || (housingTypeCriteria === 'any')) &&
-      (getPriceGrade(value.offer.price) === priceCriteria || (priceCriteria === 'any')) &&
-      (value.offer.rooms === parseInt(roomsCriteria) || (roomsCriteria === 'any')) &&
-      (value.offer.guests === parseInt(guestsCriteria) || (guestsCriteria === 'any'));
+  const filteredAds = ads.slice().filter((value) => {
+    return (value.offer.type === housingTypeCriteria || housingTypeCriteria === 'any') &&
+      (getPriceGrade(value.offer.price) === priceCriteria || priceCriteria === 'any') &&
+      (value.offer.rooms === parseInt(roomsCriteria) || roomsCriteria === 'any') &&
+      (value.offer.guests === parseInt(guestsCriteria) || guestsCriteria === 'any');
   });
+
+  filteredAds.forEach((value) => {
+    value.rank = getAdRank(value);
+  });
+
+  filteredAds.sort((adA, adB) => adB.rank - adA.rank);
+  debugger;
+  return filteredAds;
 };
 
 const setCriteria = (element) => {
@@ -52,6 +63,11 @@ const setCriteria = (element) => {
     case 'filter-washer':
     case 'filter-elevator':
     case 'filter-conditioner':
+      if (element.checked) {
+        featuresCriteria.push(element.value)
+      } else {
+        featuresCriteria.splice(featuresCriteria.indexOf(element.value), 1);
+      }
       break;
   }
 };
@@ -59,5 +75,15 @@ const setCriteria = (element) => {
 const getPriceGrade = (priceValue) => {
   return priceValue < 10000 ? 'low' : priceValue > 50000 ? 'high' : 'middle';
 };
+
+const getAdRank = (ad) => {
+  return ad.offer.features.reduce((accumulator, value) => {
+    return featuresCriteria.includes(value) ? accumulator + MATCHING_FEATURE_RANK : accumulator + BONUSE_FEATURE_RANK;
+  }, 0);
+};
+
+// const compateAds = (adA, adB) => {
+//   return adB.rank - adA.rank;
+// }
 
 export { setFilterChangeHandler, filterAds };
