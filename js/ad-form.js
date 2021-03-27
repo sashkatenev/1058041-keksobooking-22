c1ca154245207ec52ccdd2dfc8d9404ba1763851
sashkatenev <60226999+sashkatenev@users.noreply.github.com>
2021-03-27 10:54:49 +0300

    добавляет загрузку фотографии жилья

--- js/ad-form.js
+++ js/ad-form.js
@@ -17,9 +17,12 @@ const roomNumberInput = adForm.querySelector('#room_number');
 const capacityInput = adForm.querySelector('#capacity');
 const avatarInput = adForm.querySelector('#avatar');
 const avatarImage = adForm.querySelector('.ad-form-header__preview img');
+const userPictureInput = adForm.querySelector('.ad-form__input');
+const userPictureContainer = adForm.querySelector('.ad-form__photo');
 
 const adFormChangeHandler = (evt) => {
   const control = evt.target;
+  let userPictureImage = null;
 
   switch (control) {
     case housingTypeInput:
@@ -48,6 +51,13 @@ const adFormChangeHandler = (evt) => {
 
     case avatarInput:
       checkAndLoadImage(avatarInput, avatarImage);
+      break;
+
+    case userPictureInput:
+      userPictureContainer.innerHTML = '';
+      userPictureImage = document.createElement('img');
+      checkAndLoadImage(userPictureInput, userPictureImage);
+      userPictureContainer.appendChild(userPictureImage);
       break;
   }
 };
