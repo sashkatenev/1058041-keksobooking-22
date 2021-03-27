1e86d6f583a0c471300accf26e9f2d5cc948dbb7
sashkatenev <60226999+sashkatenev@users.noreply.github.com>
2021-03-27 10:50:14 +0300

    добавляет фильтрацию по цене, кол-ву комнат и гостей

--- js/filter-form.js
+++ js/filter-form.js
@@ -6,34 +6,14 @@ const FORM_CLASS_NAME = 'map__filters';
 const filterForm = document.querySelector(`.${FORM_CLASS_NAME}`);
 enableForm('map__filters', false);
 
-let housingTypeCriteria = filterForm.querySelector('#housing-type').value;
+let housingTypeCriteria  = filterForm.querySelector('#housing-type').value;
+let priceCriteria  = filterForm.querySelector('#housing-price').value;
+let roomsCriteria = filterForm.querySelector('#housing-rooms').value;
+let guestsCriteria = filterForm.querySelector('#housing-guests').value;
 
 const setFilterChangeHandler = (data, maxCount) => {
   filterForm.addEventListener('change', (evt) => {
-    const control = evt.target;
-
-    switch (control.id) {
-      case 'housing-type':
-        housingTypeCriteria = control.value;
-        break;
-
-      case 'housing-price':
-        break;
-
-      case 'housing-rooms':
-        break;
-
-      case 'housing-guests':
-        break;
-
-      case 'filter-wifi':
-      case 'filter-dishwasher':
-      case 'filter-parking':
-      case 'filter-washer':
-      case 'filter-elevator':
-      case 'filter-conditioner':
-        break;
-    }
+    setCriteria(evt.target);
     const filteredData = filterAds(data);
     showAdMarkers(filteredData, maxCount);
   });
@@ -41,8 +21,43 @@ const setFilterChangeHandler = (data, maxCount) => {
 
 const filterAds = (ads) => {
   return ads.slice().filter((value) => {
-    return (value.offer.type === housingTypeCriteria) || (housingTypeCriteria === 'any');
+    return ((value.offer.type === housingTypeCriteria) || (housingTypeCriteria === 'any')) &&
+      (getPriceGrade(value.offer.price) === priceCriteria || (priceCriteria === 'any')) &&
+      (value.offer.rooms === parseInt(roomsCriteria) || (roomsCriteria === 'any')) &&
+      (value.offer.guests === parseInt(guestsCriteria) || (guestsCriteria === 'any'));
   });
+};
+
+const setCriteria = (element) => {
+  switch (element.id) {
+    case 'housing-type':
+      housingTypeCriteria = element.value;
+      break;
+
+    case 'housing-price':
+      priceCriteria = element.value;
+      break;
+
+    case 'housing-rooms':
+      roomsCriteria = element.value;
+      break;
+
+    case 'housing-guests':
+      guestsCriteria = element.value;
+      break;
+
+    case 'filter-wifi':
+    case 'filter-dishwasher':
+    case 'filter-parking':
+    case 'filter-washer':
+    case 'filter-elevator':
+    case 'filter-conditioner':
+      break;
+  }
+};
+
+const getPriceGrade = (priceValue) => {
+  return priceValue < 10000 ? 'low' : priceValue > 50000 ? 'high' : 'middle';
 };
 
 export { setFilterChangeHandler, filterAds };
